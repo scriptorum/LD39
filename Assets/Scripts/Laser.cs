74a68bd2d709d9916ba79a6806fb98d6ba7c4881
Eric Lund <github@egotron.com>
2017-07-29 15:01:39 -0400

    Laser fire!

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+	private Rigidbody2D rb;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+    void Start()
+    {
+        rb.AddRelativeForce(new Vector2(0, 1f));
+		Destroy(this.gameObject, 1.0f);
+    }
+}
--- Assets/Scripts/TurretControls.cs
+++ Assets/Scripts/TurretControls.cs
@@ -6,20 +6,65 @@ using UnityEngine;
 
 public class TurretControls : MonoBehaviour
 {
+    public Transform laserSpawn;
+    public GameObject laserPrefab;
+
+    private bool usingGamePad = false;
+    private Vector2 lastMouse = Vector2.zero;
+    private Vector2 lastGamePad = Vector2.zero;
+    private const float FULL_CHARGE = 0.25f;
+    private float chargeTime = 0f;
 
     void Update()
     {
-		float h = Input.GetAxis("Horizontal2");
-		float v = Input.GetAxis("Vertical2");
+        chargeTime += Time.deltaTime;
 
+        lastGamePad = new Vector2(-Input.GetAxis("Horizontal2"), -Input.GetAxis("Vertical2"));
+        bool mouseButton = Input.GetMouseButton(0);
 
-		// Aim turret
-		if(h != 0f || v != 0f)
-		{
-			float z = Mathf.Atan2(-h, -v) * Mathf.Rad2Deg;
-			transform.eulerAngles = new Vector3(0f, 0f, z);
-		}
+        if (lastGamePad.x != 0f || lastGamePad.y != 0f)
+        {
+            aimTurret(lastGamePad.x, lastGamePad.y);
+            fire();
+            usingGamePad = true;
+        }
 
-		// Check for fire
+        else if (mouseButton)
+        {
+            usingGamePad = false;
+            getMousePosition();
+            aimTurret(lastMouse.x, lastMouse.y);
+            fire();
+        }
+
+        else if (!usingGamePad)
+        {
+            getMousePosition();
+            aimTurret(lastMouse.x, lastMouse.y);
+        }
+    }
+
+    private void aimTurret(float x, float y)
+    {
+        float z = Mathf.Atan2(x, y) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0f, 0f, z);
+    }
+
+    private void fire()
+    {
+        if (chargeTime < FULL_CHARGE)
+            return;
+
+        chargeTime = 0f;
+        GameObject go = (GameObject) Instantiate(laserPrefab, laserSpawn.position, transform.rotation);
+        go.transform.parent = null;
+    }
+
+    private void getMousePosition()
+    {
+        Vector3 mouse3 = Input.mousePosition;
+        mouse3 = Camera.main.ScreenToWorldPoint(mouse3);
+        lastMouse.x = transform.position.x - mouse3.x;
+        lastMouse.y = mouse3.y - transform.position.y;
     }
 }
