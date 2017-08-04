5aa625213bc93277b0e4ec9f88cb835d6f2f7034
Eric Lund <github@egotron.com>
2017-08-04 16:31:19 -0400

    Added weak positional audio to SoundManager

--- Assets/Scripts/AutoPlay.cs
+++ Assets/Scripts/AutoPlay.cs
@@ -1,12 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Spewnity;
+using UnityEngine;
 
-public class AutoPlay : MonoBehaviour {
+public class AutoPlay : MonoBehaviour
+{
+    private float elapsed = 0f;
+    private float x = -10f;
 
-	void Start()
-	{
-		SoundManager.instance.Play("song-play");
-	}
+    void Start()
+    {
+        SoundManager.instance.Play("song-play");
+    }
+
+	// PlayAt Test
+    // void Update()
+    // {
+    //     elapsed += Time.deltaTime;
+    //     if (elapsed > 1.0f)
+    //     {
+    //         elapsed = 0f;
+    //         SoundManager.instance.PlayAt("checkpoint", new Vector3(x, 0f, 0f));
+    //         x += 1.0f;
+
+    //         if (x > 10f)
+    //             x = -10f;
+    //     }
+    // }
 }
